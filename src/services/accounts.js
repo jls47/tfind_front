43f67436527c2e6c8722a477af4f6f681fbff381
jls47 <lukeschaefer@gmail.com>
2019-01-29 23:35:52 -0800

    users not accounts

--- src/services/accounts.js
+++ src/services/accounts.js
@@ -5,7 +5,7 @@ class accounts{
   static createAccount (form) {
 
     console.log(form);
-    return axios.post(`https://tfind-back.herokuapp.com/api/accounts`,form)
+    return axios.post(`https://tfind-back.herokuapp.com/api/users`,form)
     //return axios.post(`http://localhost:3000/api/users`,form)
       .then(response => {
         console.log(response);
